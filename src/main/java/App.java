d78a66e2e83f34149e9da26037406a23e008cbfa
ali <bandar3eda021@gmail.com>
2020-06-30 17:44:32 +0430

    new commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println(factorial(4));
+        System.out.println(factorial1(4));
     }
 
     public static long factorial1(long n) {
         if (n == 0 || n == 1) return 1;
-        return n * factorial(n - 1);
+        return n * factorial1(n - 1);
     }
 }
